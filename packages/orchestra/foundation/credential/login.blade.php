0700080386aa8ac47192ea17e3629436ae14313d
crynobone <crynobone@gmail.com>
2015-03-19 21:45:10 +0800

    Add forgot password views.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- packages/orchestra/foundation/credential/login.blade.php
+++ packages/orchestra/foundation/credential/login.blade.php
@@ -8,30 +8,34 @@
 		<a href="{{ handles('orchestra::/') }}">{{ memorize('site.name') }}</a>
 	</div>
 	<div class="login-box-body">
-		<p class="login-box-msg">Sign in to start your session</p>
-		{!! Form::open(['url' => handles('orchestra::login'), 'method' => 'POST']) !!}
-			<div class="form-group has-feedback">
-				<input type="text" class="form-control" placeholder="Email"/>
+		<p class="login-box-msg">{{ trans('orchestra/foundation::title.login') }}</p>
+		<form action="{{ handles('orchestra::login') }}" method="POST">
+			<input type="hidden" name="_token" value="{{ csrf_token() }}">
+			<div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
+				<input type="text" name="email" value="{{ Input::old('email') }}" required="true" tabindex="1" class="form-control" placeholder="{{ trans("orchestra/foundation::label.users.email") }}">
+				{!! $errors->first('email', '<p class="help-block">:message</p>') !!}
 			</div>
-			<div class="form-group has-feedback">
-				<input type="password" class="form-control" placeholder="Password"/>
+			<div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
+				<input type="password" name="password" class="form-control" required="true" tabindex="2" class="form-control" placeholder="{{ trans('orchestra/foundation::label.users.password') }}">
+				{!! $errors->first('password', '<p class="help-block">:message</p>') !!}
 			</div>
 			<div class="row">
 				<div class="col-xs-8">
 					<div class="checkbox">
 						<label>
-							<input type="checkbox"> Remember Me
+							<input type="checkbox" name="remember" value="yes" tabindex="3"> {{ trans('orchestra/foundation::title.remember-me') }}
 						</label>
 					</div>
 				</div>
 				<div class="col-xs-4">
-					<button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+					<button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('orchestra/foundation::title.login') }}</button>
 				</div>
 			</div>
-		{!! Form::close() !!}
+		</form>
 
-		<a href="{{ handles('orchestra::forgot') }}">I forgot my password</a><br>
-		@if (memorize('site.registrable'))
+		<a href="{{ handles('orchestra::forgot') }}">{{ trans('orchestra/foundation::title.forgot-password') }}</a>
+		<br>
+		@if (memorize('site.registrable', false))
 		<a href="{{ handles('orchestra::register') }}" class="text-center">Register a new membership</a>
 		@endif
 	</div>
--- packages/orchestra/foundation/layouts/extra.blade.php
+++ packages/orchestra/foundation/layouts/extra.blade.php
@@ -6,9 +6,6 @@
 <body{!! HTML::attributes(get_meta('html::body')) !!}>
 	@yield('content')
 
-	<!-- jQuery 2.1.3 -->
-	<script src="../../plugins/jQuery/jQuery-2.1.3.min.js"></script>
-	<!-- Bootstrap 3.3.2 JS -->
-	<script src="../../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-  </body>
+    @include('orchestra/foundation::layouts._footer')
+</body>
 </html>
