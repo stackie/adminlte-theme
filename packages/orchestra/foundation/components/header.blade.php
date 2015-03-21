611614c2a7c67d2603d7bb669a55952b618d0fea
crynobone <crynobone@gmail.com>
2015-03-21 11:21:16 +0800

    Working on form elements.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- packages/orchestra/foundation/components/header.blade.php
+++ packages/orchestra/foundation/components/header.blade.php
@@ -4,10 +4,17 @@ $title = get_meta('title');
 $description = get_meta('description'); ?>
 
 <section class="content-header">
-    <h1>
-        {!! $title or '' !!}
-        @if (! empty($description))
-        <small>{!! $description or '' !!}</small>
-        @endif
-    </h1>
+	@if (get_meta('header::add-button'))
+	<div class="pull-right">
+		<a href="{!! app('url')->current() !!}/create" class="btn btn-primary">
+			{{ trans('orchestra/foundation::label.add') }}
+		</a>
+	</div>
+	@endif
+	<h1>
+		{!! $title or '' !!}
+		@if (! empty($description))
+		<small>{!! $description or '' !!}</small>
+		@endif
+	</h1>
 </section>
--- packages/orchestra/foundation/dashboard/index.blade.php
+++ packages/orchestra/foundation/dashboard/index.blade.php
@@ -2,28 +2,28 @@
 
 @section('content')
 <div class="row">
-    @if (count($panes) > 0)
+	@if (count($panes) > 0)
 
-    <?php $panes->add('mini-profile', '<')->title('Mini Profile')
-        ->attributes(['class' => 'three columns widget'])
-        ->content(view('orchestra/foundation::components.miniprofile')); ?>
+	<?php $panes->add('mini-profile', '<')->title('Mini Profile')
+		->attributes(['class' => 'three columns widget'])
+		->content(view('orchestra/foundation::components.miniprofile')); ?>
 
-    @foreach ($panes as $id => $pane)
-        <?php $attributes = app('html')->decorate($pane->attributes, ['class' => 'panel']); ?>
-        <div{!! app('html')->attributes($attributes) !!}>
-        @if (! empty($pane->html))
-        {!! $pane->html !!}
-        @else
-        <div class="panel-heading">
-            {!! $pane->title !!}
-        </div>
-        {!! $pane->content !!}
-        @endif
-        </div>
-    @endforeach
-    @else
-    @include('orchestra/foundation::dashboard._welcome')
-    @endif
+	@foreach ($panes as $id => $pane)
+		<?php $attributes = app('html')->decorate($pane->attributes, ['class' => 'panel']); ?>
+		<div{!! app('html')->attributes($attributes) !!}>
+		@if (! empty($pane->html))
+		{!! $pane->html !!}
+		@else
+		<div class="panel-heading">
+			{!! $pane->title !!}
+		</div>
+		{!! $pane->content !!}
+		@endif
+		</div>
+	@endforeach
+	@else
+	@include('orchestra/foundation::dashboard._welcome')
+	@endif
 </div>
 
 @stop
